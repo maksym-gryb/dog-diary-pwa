cac2271c0a0936fc50b4b0562c1bfee0a7568275
maksym-gryb <maksym.gryb@gmail.com>
2026-06-11 22:03:35 -0700

    fully working version with auth

--- app.js
+++ app.js
@@ -1,52 +1,187 @@
-
+const login_section = document.getElementById("login_section");
 const calendar = document.getElementById("calendar");
 const day = document.getElementById("day");
 
 const project = "dog-diary-pwa";
 const dbname = "diary";
-const storename = "activities";
+const storename = "diary";
 
 const today = new Date();
+const currentDay = today.getDay();
 const currentMonth = today.getMonth();
 const currentYear = today.getFullYear();
 
 const startingData = [
-  {"date": "2026-06-09", "time": "06:30", "activity":"pee"}
+  { "date": "2026-06-09", "time": "06:30", "activity": "pee" }
 ];
 
-/****************************** DB SETUP ******************************/
-let db;
-const request = indexedDB.open(dbname);
-request.onerror = (event) => {
-  console.error(`Database error: ${event.target.error?.message}`);
+const activities = ["pee", "poop", "toothbrush", "bath"];
 
-};
-request.onsuccess = (event) => {
-  db = event.target.result;
-};
-// This event is only implemented in recent browsers
-request.onupgradeneeded = (event) => {
-  // Save the IDBDatabase interface
-  const db = event.target.result;
+const LegacyActivityMap = Object.freeze({
+  peeing: "pee",
+  pooping: "poop"
+});
 
-  // Create an objectStore for this database
-  const objectStore = db.createObjectStore(storename, { autoIncrement: true });
+const ActivityIcon = Object.freeze({
+  pee: "fa-toilet",
+  poop: "fa-poo",
+  walk: "fa-person-walking",
+  eat: "fa-bone",
+  drink: "fa-mug-hot",
+  sleep: "fa-bed",
 
-  objectStore.createIndex("date", "date", { unique: false });
-  // objectStore.createIndex("time", "time", { unique: false });
+  toothbrush: "fa-tooth",
+  bath: "fa-bath"
+});
 
-  // Use transaction oncomplete to make sure the objectStore creation is
-  // finished before adding data into it.
-  objectStore.transaction.oncomplete = (event) => {
-    // Store values in the newly created objectStore.
-    const customerObjectStore = db
-      .transaction(storename, "readwrite")
-      .objectStore(storename);
-    startingData.forEach((act) => {
-      customerObjectStore.add(act);
-    });
+function createIcon(activityType) {
+  const icon = document.createElement("i");
+
+  icon.classList.add("fa-solid");
+
+  let t = activityType;
+  if(!activities.includes(t)) {
+    t = LegacyActivityMap[t]
   }
-};
+
+  const specific = ActivityIcon[t] || "fa-question";
+  icon.classList.add(specific);
+
+  return icon;
+}
+
+let data_set = []
+
+/****************************** DB SETUP ******************************/
+// Initialize Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+import { 
+  collection,
+  query,
+  where,
+  doc,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+import { 
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  setPersistence,
+  browserLocalPersistence
+}
+  from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+
+let unsubscribe = null
+
+function listenByDate(db, queryDate) {
+  day.innerHTML = "<div class='spinner'/>";
+
+  document.querySelectorAll(".calendar-date-selected").forEach(e => {
+    e.classList.remove("calendar-date-selected");
+  });
+
+  const selectedElement = document.getElementById(queryDate);
+  selectedElement.classList.add("calendar-date-selected");
+
+  // stop old listener
+  if (unsubscribe) {
+    unsubscribe();
+  }
+
+  const q = query(
+    collection(db, "diary"),
+    where("date", "==", queryDate)
+  );
+
+  unsubscribe = onSnapshot(q, (snapshot) => {
+    const entries = snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+
+    data_set = entries;
+
+    console.log("Today updated:", entries);
+    renderDay(queryDate, entries);
+  });
+}
+
+// async function loadToday() {
+//   const q = query(
+//     collection(db, "diary"),
+//     where("date", "==", "2026-06-09")
+//   );
+
+//   const snapshot = await getDocs(q);
+
+//   snapshot.forEach((docSnap) => {
+//     console.log(docSnap.data());
+//   });
+
+//   return snapshot.docs.map(s => s.data());
+// }
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import { firebaseConfig } from "./config.local.js";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+let userCredential = null;
+
+// Enable persistent login
+await setPersistence(auth, browserLocalPersistence);
+
+
+// const analytics = getAnalytics(app);
+
+const db = getFirestore(app)
+
+// let db;
+// const request = indexedDB.open(dbname);
+// request.onerror = (event) => {
+//   console.error(`Database error: ${event.target.error?.message}`);
+
+// };
+// request.onsuccess = (event) => {
+//   db = event.target.result;
+// };
+// // This event is only implemented in recent browsers
+// request.onupgradeneeded = (event) => {
+//   // Save the IDBDatabase interface
+//   const db = event.target.result;
+
+//   // Create an objectStore for this database
+//   const objectStore = db.createObjectStore(storename, { autoIncrement: true });
+
+//   objectStore.createIndex("date", "date", { unique: false });
+//   // objectStore.createIndex("time", "time", { unique: false });
+
+//   // Use transaction oncomplete to make sure the objectStore creation is
+//   // finished before adding data into it.
+//   objectStore.transaction.oncomplete = (event) => {
+//     // Store values in the newly created objectStore.
+//     const customerObjectStore = db
+//       .transaction(storename, "readwrite")
+//       .objectStore(storename);
+//     startingData.forEach((act) => {
+//       customerObjectStore.add(act);
+//     });
+//   }
+// };
 /****************************** END DB SETUP ******************************/
 
 function renderCalendar(month, year) {
@@ -55,12 +190,15 @@ function renderCalendar(month, year) {
   const daysInMonth = new Date(year, month, 0).getDate();
   const daysInLastMonth = new Date(year, month - 1, 0).getDate();
 
-  // 7 x 5 grid -> 35
-  for (let i = 0; i < 35; i++) {
+  for (let i = 0; i < 7 * 5; i++) {
 
     let thisDay = new Date(year, month);
     thisDay.setDate(thisDay.getDate() + i - firstDayOfMonth)
     const d = thisDay.getDate();
+    
+    const isCurrentDay =
+      currentYear == thisDay.getFullYear() && currentMonth == thisDay.getMonth() && currentDay == thisDay.getDate();
+
     const d_str = thisDay.toISOString().split('T')[0]
 
     if (i != 0 && i % 7 == 0) {
@@ -69,38 +207,122 @@ function renderCalendar(month, year) {
 
 
     const e = document.createElement("div");
+    e.id = d_str;
     e.textContent = d;
-    e.className = "calendar-date";
-    e.onclick = function () { renderDay(d_str) };
+    e.classList.add("calendar-date");
+    if(isCurrentDay) {
+      e.classList.add("calendar-date-today");
+    }
+    e.onclick = function () { listenByDate(db, d_str) };
     calendar.appendChild(e);
   }
 }
 
-function renderDay(renderDate) {
+async function renderDay(renderDate, all_data) {
   day.textContent = "";
 
-  const store = db.transaction(storename, "readonly").objectStore(storename);
-  const index = store.index("date");
+  // const all_data = await loadToday();
 
-  const keyRange = IDBKeyRange.only(renderDate);
-  const query = index.getAll(keyRange);
+  // const store = db.transaction(storename, "readonly").objectStore(storename);
+  // const index = store.index("date");
+
+  // const keyRange = IDBKeyRange.only(renderDate);
+  // const query = index.getAll(keyRange);
 
 
-  query.onsuccess = () => {
-    for (let i = 0; i < 48; i++) {
-      const timeslot = `${leftPad(Math.floor(i / 2).toString())}:${leftPad((i % 2 * 30).toString())}`;
+  // query.onsuccess = () => {
+  for (let i = 0; i < 48; i++) {
+    const timeslot = `${leftPad(Math.floor(i / 2).toString())}:${leftPad((i % 2 * 30).toString())}`;
 
-      const slot_data = query.result.filter(l => l.time == timeslot)
+    const slot_data = all_data.filter(l => l.time == timeslot)
 
-      const e = document.createElement("div");
-      e.id = timeslot.replace(":", "_");
-      e.className = "slot";
-      e.textContent = `${timeslot} - ${slot_data.map(l => l.activity)}`
-      day.appendChild(e);
+    const e = document.createElement("div");
+    e.id = timeslot.replace(":", "_");
+    e.className = "slot";
+    // e.textContent = `${timeslot} - ${slot_data.map(l => l.activity)}`
+    const acts = activities.map(a => {
+      const dv = document.createElement("div");
+      const input = document.createElement("input");
+      input.type = "checkbox";
+      input.checked = slot_data.filter(l => l.activity == a).length > 0 ? true : false;
+
+      input.addEventListener("click", async (e) => {
+        e.stopPropagation();
+
+        await toggleActivity(timeslot, renderDate, a)
+      });
+
+      dv.classList.add("activity_checkbox")
+      if (input.checked) {
+        dv.classList.add("checked")
+      }
+
+      dv.innerHTML = `${ActivityIcon[a]}: `;
+      dv.appendChild(input)
+      dv.appendChild(createIcon(a))
+      return dv;
+    });
+    e.innerHTML = `<b>${timeslot}</b>`
+    acts.forEach(act => e.appendChild(act));
+    // e.onclick = function () { toggleActivity(timeslot, renderDate) }
+    e.onclick = function () { revealSlotOptions(e.id) }
+    day.appendChild(e);
+  }
+
+  // window.location.hash = "";
+  // window.location.hash = "#10_30";
+  // }
+}
+
+async function toggleActivity(timeslot, date, activity) {
+  try {
+    let existingData = [];
+    if (data_set.length > 0) {
+      existingData = data_set.filter(x => 
+        x.time == timeslot && x.date == date && x.activity == activity);
     }
 
-    window.location.hash = "";
-    window.location.hash = "#10_30";
+    if(existingData && existingData.length > 0) {
+      await Promise.all(
+        existingData.map(d => deleteDoc(doc(db, storename, d.id)))
+      );
+      console.log("Deleted ids: ", existingData.map(d => d.id).join(" , "));
+    }
+    else {
+      const docRef = await addDoc(collection(db, storename), {
+        date: date,
+        time: timeslot,
+        activity: activity,
+        creator: auth.currentUser.uid
+      });
+
+    console.log("Saved with ID:", docRef.id);
+    }
+  } catch (e) {
+    console.error("Error adding document:", e);
+  }
+}
+
+function revealSlotOptions(slot_id) {
+  console.log(slot_id)
+
+  const slot = document.getElementById(slot_id);
+  const olds = document.getElementsByClassName("slot_select")
+
+  let isReSelected = false;
+  for (const l of olds) {
+    if(l.id == slot.id) {
+      isReSelected = true;
+      break;
+    }
+  }
+
+  while (olds.length > 0 && olds[0].classList.contains("slot_select")) {
+    olds[0].classList.remove("slot_select")
+  }
+
+  if(!isReSelected) {
+    slot.classList.add("slot_select")
   }
 }
 
@@ -109,10 +331,37 @@ function renderDay(renderDate) {
 */
 function leftPad(i) {
   if (i.length == 2) return i;
-  return "0"+i;
+  return "0" + i;
 }
 
-renderCalendar(currentMonth, currentYear);
+// Login button
+document.getElementById("loginBtn").addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    userCredential = user;
+    console.log(user)
+    login_section.innerHTML = `<p>Logged in as: ${user.email}</p>`
+    renderCalendar(currentMonth, currentYear);
+  } else {
+    calendar.innerText = "Not logged in";
+  }
+});
+
+
+
+// if(login()){
+//   renderCalendar(currentMonth, currentYear);
+// }
 
 
 // const newPeriodFormEl = document.getElementsByTagName("form")[0];
